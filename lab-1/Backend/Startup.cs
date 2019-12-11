b99199c75207ba8ec7ce26fde6d55f7f4c188eee
Valentine Radchuk <valentine.radchuk@outlook.com>
2019-12-11 16:43:53 +0200

    Apply CORS

--- lab-1/Backend/Startup.cs
+++ lab-1/Backend/Startup.cs
@@ -25,6 +25,7 @@ namespace Backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
